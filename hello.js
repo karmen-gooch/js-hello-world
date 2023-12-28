97cd1d8e2a96853a37d493dfc4216c7f93ce079f
Karmen Gooch <karmengooch@MacBook-Pro.ht.home>
2023-12-28 01:22:13 -0600

    now says Hello, Big Beautiful World

--- hello.js
+++ hello.js
@@ -4,14 +4,21 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log('Hello, Big Beautiful World!');
+const num1 = 5;
+const num2 = 3;
+const sum = num1 + num2;
+console.log('The sum of ' + num1 + ' and ' +num2 + ' is: ' +sum);
+console.log('My name is Karmen Gooch');
+console.log('What is your name?');
+console.log('Quoth the Raven "Nevermore"');
+console.log('Karma\'s World' );
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
+   2. Figure out what the parentheses do. Will the code work without them? No it doesnt work the the parentheses 
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
    4. Remove the semi-colon, ;.
    5. Print a number. (Bonus: Print two numbers added together).
